1609d0ce746e66e12935df69d777f72191d4bf71
atila <atilamtmoretto@gmail.com>
2023-03-29 09:40:03 +0200

    Make threshold configuration functions return boolean to indicate success.

--- src/test/mock_thermometer.h
+++ src/test/mock_thermometer.h
@@ -4,5 +4,6 @@ class MockThermometer: public Thermometer {
 public:
 
     MOCK_METHOD(int, GetTemperature, (), (const, override));
-    MOCK_METHOD(void, SetTemperatureThresholds, (int high, int low), (override));
+    MOCK_METHOD(bool, SetTemperatureThresholds, (int high, int low), (override));
+    MOCK_METHOD(void, RegisterCallback, (void(*callback)(bool isHigh)), (override));
 };
--- src/thermometer.h
+++ src/thermometer.h
@@ -22,10 +22,11 @@ public:
     /**
      * @brief Configures new values for high and low temperature thresholds. If the current temperature
      *        is exceeding either threshold, the temperatureThresholdReachedCallback is immediately triggered.
-     * @param high The new high temperature threshold.
-     * @param low the new low temperature threshold.
+     * @param high The new high temperature threshold. If greater than the max measurable temperature, the max is used.
+     * @param low the new low temperature threshold. If lower than the min measurable temperature, the min is used.
+     * @ret   True if the setting is accepted (low < high), false otherwise
      */
-    virtual void SetTemperatureThresholds(int high, int low) = 0;
+    virtual bool SetTemperatureThresholds(int high, int low) = 0;
 
     /**
      * @brief Configures a callback to be called when the temperature raises above the high threshold or below the low
